e0588f560d1a29a07c1134afe6a5e4cb93f3e03e
Ifenna-Pascal <71331395+Ifenna-Pascal@users.noreply.github.com>
2021-08-23 15:28:10 +0100

    resolved conflicts

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -14,11 +14,7 @@ const reducers = combineReducers({
   adminInfo: adminDataReducer,
   liveTrade : admin_live_trade,
   stock: stockReducer,
-<<<<<<< HEAD
-  toggle:ToggleOpenReducer
-=======
   theme: darkThemeReducer,
->>>>>>> 718d8be4c8445edaf5ab0fc73ddb69a34cc0cdb1
 });
 
 export default reducers;
