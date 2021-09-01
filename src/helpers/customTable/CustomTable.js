99d448bd326c083cea6fb9e6f259916d85a33da9
hobbyluv <princewillchime43@gmail.com>
2021-09-01 17:53:20 +0100

    removed unwanted data

--- src/helpers/customTable/CustomTable.js
+++ src/helpers/customTable/CustomTable.js
@@ -1,19 +1,8 @@
 import * as React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { DataGrid } from "@material-ui/data-grid";
-import { useDispatch, useSelector } from "react-redux";
-import { PROCESS_DEPOSIT } from "../../store/action-types";
-// import {
-//   randomCreatedDate,
-//   randomTraderName,
-//   randomUpdatedDate,
-// } from "@material-ui/x-grid-data-generator";
 
 const useStyles = makeStyles((theme) => {
-  // const backgroundColor =
-  //   getThemePaletteMode(theme.palette) === "dark"
-  //     ? "#376331"
-  //     : "rgb(217 243 190)";
   return {
     root: {
       backgroundColor: "#fff",
@@ -39,22 +28,8 @@ const CustomTable = ({
 
   const classes = useStyles();
   const [pageSize, setPageSize] = React.useState(5);
-  const dispatch = useDispatch();
+
   // const {allUsers} = useSelector(state => state.profile)
-
-  const handleCellEditCommit = React.useCallback(({ id, field, value }) => {
-    if (field === "fullName") {
-      const [firstName, lastName] = value.toString().split(" ");
-      const updatedRows = dataRows.map((row) => {
-        if (row.id === id) {
-          return { ...row, firstName, lastName };
-        }
-        return dataRows;
-      });
-
-      dispatch({ type: PROCESS_DEPOSIT, payload: updatedRows });
-    }
-  }, []);
 
   console.log(`dataRows`, dataRows);
   return (
